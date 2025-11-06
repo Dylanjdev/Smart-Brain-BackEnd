cce3c7007304d5a852764f6d295bf7b0a45bac35
Dylan Smith <dylanjsmith.dev@gmail.com>
2025-11-06 11:27:50 -0500

    smartbrain backend

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./Controllers/register');
+const signin = require('./Controllers/signin')
+const profile = require('./Controllers/profile')
+const image = require('./Controllers/image')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'postgres',
+        password: 'Dylanjdev',
+        database: 'smart-brain'
+    }
+});
+
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.get('/', (req, res)=> {
+    res.send('success')
+})
+
+app.post('/signin', signin.handleSignin(db, bcrypt))
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000')
+})
